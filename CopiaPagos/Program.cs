df673b2a8331d4517ee5d99b512ecee369818bd4
DesarrolloGNA <desarrollo@gna.cl>
2022-06-29 09:15:54 -0400

    Modificacion de Rutas
    Se cambia nuevas ruta y ejecucion del nombre del archivo

--- CopiaPagos/Program.cs
+++ CopiaPagos/Program.cs
@@ -11,39 +11,80 @@ namespace CopiaPagos
         static void Main(string[] args)
         {
 
-            string Fecha = DateTime.Now.ToString("dd-MM-yyyy");
             string Ruta_Origen = @"C:\Coopeuch\Pagos\";
-            string Ruta_Destino = @"\\192.168.0.227\COO_Coopeuch\02.Pagos\";
-            string Nombre_Archivo = "Recupero_castigo_GARCIA NADAL_" + Fecha + ".xlsx.xlsx";
-            string Ruta_Destino_historial = @"C:\coopeuch\pagos\his\" + Fecha;
+            string Ruta_Destino = @"\\192.168.0.227\COO_Coopeuch\02.Pagos\";     
+            string Ruta_Destino_historial = @"C:\coopeuch\pagos\his\";
 
             //Valido que el directorio este disponible
             while (!System.IO.Directory.Exists(Ruta_Destino)) { }
 
-            if (System.IO.File.Exists((Ruta_Origen + Nombre_Archivo)))
+            List<string> strFiles = System.IO.Directory.GetFiles(Ruta_Origen, "*", System.IO.SearchOption.AllDirectories).ToList();
+
+            foreach (string fichero in strFiles)
             {
-                //Copio Tecnocob
-                if (!System.IO.File.Exists((Ruta_Destino + Nombre_Archivo)))
-                {
 
-                    System.IO.File.Copy((Ruta_Origen + Nombre_Archivo), (Ruta_Destino + Nombre_Archivo));
+                if (System.IO.Path.GetFileName(fichero).Contains("Recupero_castigo_GARCIA NADAL")) {
+
+
+
+                    if (!System.IO.File.Exists((Ruta_Destino_historial + System.IO.Path.GetFileName(fichero))))
+                    {
+
+                        //Copio Historico
+                        System.IO.File.Copy((fichero), Ruta_Destino_historial + System.IO.Path.GetFileName(fichero));
+
+                        //Copio Tecnocob
+                        System.IO.File.Copy(fichero, (Ruta_Destino + System.IO.Path.GetFileName(fichero)));
+
+                        //Elimino El archivo original
+                        System.IO.File.Delete(fichero);
+
+                    }
+
+
+
                 }
 
-                //Copio Historico
-                if (!System.IO.File.Exists((Ruta_Origen + Ruta_Destino_historial)))
+                if (System.IO.Path.GetFileName(fichero).Contains("Recupero_castigo_judicial"))
                 {
+                    if (!System.IO.File.Exists((Ruta_Destino_historial + System.IO.Path.GetFileName(fichero))))
+                    {
 
-                    System.IO.Directory.CreateDirectory(Ruta_Destino_historial);
+                        //Copio Historico
+                        System.IO.File.Copy((fichero), Ruta_Destino_historial + "\\" + System.IO.Path.GetFileName(fichero));
 
-                    var path = (Ruta_Destino_historial + "\\" + Nombre_Archivo);
-
-                    System.IO.File.Copy((Ruta_Origen + Nombre_Archivo), path);
+                        //Elimino El archivo original
+                        System.IO.File.Delete(fichero);
+                    }
                 }
 
-                //Elimino El archivo original
-                System.IO.File.Delete((Ruta_Origen + Nombre_Archivo));
             }
+
+
+            //if (System.IO.File.Exists((Ruta_Origen + Nombre_Archivo)))
+            //{
+            //    //Copio Tecnocob
+            //    if (!System.IO.File.Exists((Ruta_Destino + Nombre_Archivo)))
+            //    {
+
+            //        System.IO.File.Copy((Ruta_Origen + Nombre_Archivo), (Ruta_Destino + Nombre_Archivo));
+            //    }
+
+            //    //Copio Historico
+            //    if (!System.IO.File.Exists((Ruta_Origen + Ruta_Destino_historial)))
+            //    {
+
+            //        System.IO.Directory.CreateDirectory(Ruta_Destino_historial);
+
+            //        var path = (Ruta_Destino_historial + "\\" + Nombre_Archivo);
+
+            //        System.IO.File.Copy((Ruta_Origen + Nombre_Archivo), path);
+            //    }
+
+            //    //Elimino El archivo original
+            //    System.IO.File.Delete((Ruta_Origen + Nombre_Archivo));
+        }
 
         }
     }
-}
+//}
