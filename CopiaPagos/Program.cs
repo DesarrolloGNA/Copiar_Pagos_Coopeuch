5c5f8980aadb1853ccc946db3d16149660888bbd
DesarrolloGNA <desarrollo@gna.cl>
2022-09-07 15:21:26 -0400

    actualizacion

--- CopiaPagos/Program.cs
+++ CopiaPagos/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,81 +11,81 @@ namespace CopiaPagos
     {
         static void Main(string[] args)
         {
-
-            string Ruta_Origen = @"C:\Coopeuch\Pagos\";
-            string Ruta_Destino = @"\\192.168.0.227\COO_Coopeuch\02.Pagos\";     
-            string Ruta_Destino_historial = @"C:\coopeuch\pagos\his\";
-
-            //Valido que el directorio este disponible
-            while (!System.IO.Directory.Exists(Ruta_Destino)) { }
-
-            List<string> strFiles = System.IO.Directory.GetFiles(Ruta_Origen, "*", System.IO.SearchOption.AllDirectories).ToList();
-
-            foreach (string fichero in strFiles)
+            try
             {
 
-                if (System.IO.Path.GetFileName(fichero).Contains("Recupero_castigo_GARCIA NADAL")) {
+
+                string Ruta_Origen = @"C:\Coopeuch\Pagos\";
+                string Ruta_Destino = @"\\192.168.0.227\COO_Coopeuch\02.Pagos\";     
+                string Ruta_Destino_historial = @"C:\coopeuch\pagos\his\";
+
+                //string strCmdText;
+                //strCmdText = @"net use \\192.168.0.227\CMR_Falabella /user:gna_user Tecno.202x";
+                //System.Diagnostics.Process.Start("CMD.exe", strCmdText);
 
 
-
-                    if (!System.IO.File.Exists((Ruta_Destino_historial + System.IO.Path.GetFileName(fichero))))
-                    {
-
-                        //Copio Historico
-                        System.IO.File.Copy((fichero), Ruta_Destino_historial + System.IO.Path.GetFileName(fichero));
-
-                        //Copio Tecnocob
-                        System.IO.File.Copy(fichero, (Ruta_Destino + System.IO.Path.GetFileName(fichero)));
-
-                        //Elimino El archivo original
-                        System.IO.File.Delete(fichero);
-
-                    }
-
-
-
-                }
-
-                if (System.IO.Path.GetFileName(fichero).Contains("Recupero_castigo_judicial"))
+                //Valido que el directorio este disponible
+                while (!Directory.Exists(Ruta_Destino))
                 {
-                    if (!System.IO.File.Exists((Ruta_Destino_historial + System.IO.Path.GetFileName(fichero))))
-                    {
-
-                        //Copio Historico
-                        System.IO.File.Copy((fichero), Ruta_Destino_historial + "\\" + System.IO.Path.GetFileName(fichero));
-
-                        //Elimino El archivo original
-                        System.IO.File.Delete(fichero);
-                    }
+                    Console.WriteLine("valida directorio");
                 }
+                Console.WriteLine("paso el valida directorio");
+                List<string> strFiles = Directory.GetFiles(Ruta_Origen, "*", SearchOption.AllDirectories).ToList();
+
+                foreach (string fichero in strFiles)
+                {
+
+                    if (Path.GetFileName(fichero).Contains("Recupero_castigo_GARCIA NADAL")) {
+
+
+
+                        if (!File.Exists((Ruta_Destino_historial + Path.GetFileName(fichero))))
+                        {
+
+                            //Copio Historico
+                            File.Copy((fichero), Ruta_Destino_historial + Path.GetFileName(fichero));
+
+                            //Copio Tecnocob
+                            File.Copy(fichero, (Ruta_Destino + Path.GetFileName(fichero)));
+
+                            //Elimino El archivo original
+                            File.Delete(fichero);
+
+                        }
+
+
+
+                    }
+
+                    if (Path.GetFileName(fichero).Contains("Recupero_castigo_judicial"))
+                    {
+                        if (!File.Exists((Ruta_Destino_historial + Path.GetFileName(fichero))))
+                        {
+
+                            //Copio Historico
+                           File.Copy((fichero), Ruta_Destino_historial + "\\" + Path.GetFileName(fichero));
+
+                            //Elimino El archivo original
+                            File.Delete(fichero);
+                        }
+                    }
+
+                }
+
+
+            }
+            catch (Exception EX)
+            {
+
+                Console.WriteLine(EX.Message.ToString());
+          
 
             }
 
 
-            //if (System.IO.File.Exists((Ruta_Origen + Nombre_Archivo)))
-            //{
-            //    //Copio Tecnocob
-            //    if (!System.IO.File.Exists((Ruta_Destino + Nombre_Archivo)))
-            //    {
-
-            //        System.IO.File.Copy((Ruta_Origen + Nombre_Archivo), (Ruta_Destino + Nombre_Archivo));
-            //    }
-
-            //    //Copio Historico
-            //    if (!System.IO.File.Exists((Ruta_Origen + Ruta_Destino_historial)))
-            //    {
-
-            //        System.IO.Directory.CreateDirectory(Ruta_Destino_historial);
-
-            //        var path = (Ruta_Destino_historial + "\\" + Nombre_Archivo);
-
-            //        System.IO.File.Copy((Ruta_Origen + Nombre_Archivo), path);
-            //    }
-
-            //    //Elimino El archivo original
-            //    System.IO.File.Delete((Ruta_Origen + Nombre_Archivo));
-        }
 
         }
+
     }
-//}
+}
+
